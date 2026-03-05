0d3e75549690faa8d0027ebab1c7fb4d3ef1a4ec
thecryptohunter-boop <thecryptohunter@gmail.com>
2026-03-05 16:50:39 +0300

    Update scheduler.py

--- modules/scheduler.py
+++ modules/scheduler.py
@@ -18,7 +18,7 @@ async def scheduler(post_today, bot, CHANNEL_ID, ADMINS):
 
         # TODAY
 
-        if now.hour == 16 and now.minute == 36:
+        if now.hour == 16 and now.minute == 56:
 
             if last_today != now.date():
 
