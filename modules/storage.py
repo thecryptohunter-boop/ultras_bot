0f876c5b1d0806e8b26179e23ccc672b4b88d7b9
thecryptohunter-boop <thecryptohunter@gmail.com>
2026-03-05 13:16:55 +0300

    Create storage.py

---
+++ modules/storage.py
@@ -0,0 +1,15 @@
+import json
+
+FILE = "data/categories.json"
+
+
+def load_categories():
+
+    with open(FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def save_categories(data):
+
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
