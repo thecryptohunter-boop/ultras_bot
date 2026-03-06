50669617257c3ba18f7d9d3ae0a10aba9d9a83c8
thecryptohunter-boop <thecryptohunter@gmail.com>
2026-03-07 00:32:24 +0300

    Create config.py

---
+++ modules/config.py
@@ -0,0 +1,12 @@
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+ADMINS = {334306921}
+
+TIMEZONE = "Europe/Moscow"
+
+TODAY_POST_HOUR = 17
+TODAY_POST_MINUTE = 30
