cbbd43dc61e7bffb3ec3304ce9c323e7e3a762b8
thecryptohunter-boop <thecryptohunter@gmail.com>
2026-03-19 16:32:09 +0300

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -3,7 +3,7 @@ import os
 TOKEN = os.getenv("TOKEN")
 
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
-
+GROUP_ID = int(os.getenv("GROUP_ID"))  # твоя группа
 ADMINS = {334306921}
 
 TIMEZONE = "Europe/Moscow"
