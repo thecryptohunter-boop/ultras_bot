d3e36499548a9354eea59a2171e400f2f0f94265
thecryptohunter-boop <thecryptohunter@gmail.com>
2026-03-26 22:51:54 +0300

    Update quiz_engine.py

--- modules/quiz_engine.py
+++ modules/quiz_engine.py
@@ -152,9 +152,9 @@ class QuizEngine:
 
         index = self.state["question_index"]
         correct = self.questions[index]["correct"]
-
+    
         correct_users = []
-
+    
         for uid, data in self.state["answer_times"].items():
             if data["option"] == correct:
                 correct_users.append({
@@ -162,49 +162,50 @@ class QuizEngine:
                     "name": data["name"],
                     "time": data["time"]
                 })
-
+    
         sorted_users = sorted(correct_users, key=lambda x: x["time"])
         top3 = sorted_users[:3]
-
+    
         points = [3, 2, 1]
         medals = ["🥇", "🥈", "🥉"]
-
+    
         text = f"❓ <b>Вопрос {index+1} завершён</b>\n\n"
-
+    
         # ТОП игроков
         if not top3:
             text += "😬 Никто не угадал\n"
         else:
             for i, user in enumerate(top3):
+    
                 text += f"{medals[i]} {user['name']} (+{points[i]})\n"
-
+    
                 uid = user["uid"]
-
+    
                 if uid not in self.state["scoreboard"]:
                     self.state["scoreboard"][uid] = {
                         "name": user["name"],
                         "score": 0
                     }
-                
+    
                 self.state["scoreboard"][uid]["score"] += points[i]
-
+    
         # правильный ответ
         correct_option = self.questions[index]["options"][correct]
         text += f"\n✅ <b>{correct_option}</b>\n🤝 <i><b>Sponsored by Et Vivit</b></i>"
-        
+    
         # рейтинг
         scores = sorted(
             self.state["scoreboard"].items(),
             key=lambda x: x[1]["score"],
             reverse=True
         )
-
+    
         if scores:
-            text += "\n\n\n🏆 <b>Рейтинг:</b>\n"
-
+            text += "\n\n🏆 <b>Рейтинг:</b>\n"
+    
             for i, (uid, data) in enumerate(scores[:5]):
                 text += f"{i+1}. {data['name']} — <b>{data['score']}</b>\n"
-
+    
         await self.bot.send_message(self.group_id, text)
 
     # ===== FINISH QUIZ =====
