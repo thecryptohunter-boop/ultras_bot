ba425052d7a9dc24523475ae0850c1715359aa64
thecryptohunter-boop <thecryptohunter@gmail.com>
2026-03-26 20:26:45 +0300

    Update quiz_engine.py

--- modules/quiz_engine.py
+++ modules/quiz_engine.py
@@ -2,6 +2,11 @@ import asyncio
 import time
 from aiogram import Bot
 from modules.quiz_storage import load_questions, load_results, save_results
+from PIL import Image, ImageDraw, ImageFont
+import requests
+from io import BytesIO
+
+
 
 
 class QuizEngine:
@@ -59,6 +64,55 @@ class QuizEngine:
         await asyncio.sleep(1)
 
         asyncio.create_task(self.send_question())
+
+    # ===== СТАТИСТИКА
+    
+    async def create_scoreboard_image(bot, top_players):
+
+        width = 800
+        height = 400
+    
+        img = Image.new("RGB", (width, height), color=(20, 20, 20))
+        draw = ImageDraw.Draw(img)
+    
+        font = ImageFont.load_default()
+    
+        y = 50
+    
+        medals = ["🥇", "🥈", "🥉"]
+    
+        for i, (user_id, name, score) in enumerate(top_players[:3]):
+    
+            # получаем аватар
+            try:
+                photos = await bot.get_user_profile_photos(user_id)
+    
+                file_id = photos.photos[0][0].file_id
+                file = await bot.get_file(file_id)
+    
+                file_url = f"https://api.telegram.org/file/bot{bot.token}/{file.file_path}"
+    
+                response = requests.get(file_url)
+                avatar = Image.open(BytesIO(response.content)).resize((80, 80))
+    
+            except:
+                avatar = Image.new("RGB", (80, 80), (100, 100, 100))
+    
+            img.paste(avatar, (50, y))
+    
+            text = f"{medals[i]} {name} — {score}"
+    
+            draw.text((150, y + 25), text, fill=(255, 255, 255), font=font)
+    
+            y += 100
+    
+        output = BytesIO()
+        output.name = "scoreboard.png"
+    
+        img.save(output, format="PNG")
+        output.seek(0)
+    
+        return output
 
     # ===== SEND QUESTION =====
 
